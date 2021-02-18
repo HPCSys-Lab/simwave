68a689c564a62d4c70d15bdf2d5cdc0bb5a875e8
Keith Roberts <krober@usp.edu>
2021-02-17 23:38:31 -0300

    provide kwarg to fix clim when plotting

--- pywave/plots/plot.py
+++ pywave/plots/plot.py
@@ -4,10 +4,18 @@ import os
 from matplotlib import cm
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-def plot_wavefield(wavefield, file_name='wavefield', colorbar=True, cmap="gray", show=False):
+
+def plot_wavefield(
+    wavefield,
+    file_name="wavefield",
+    colorbar=True,
+    cmap="gray",
+    show=False,
+    clim=[-5, 5],
+):
 
     # create the destination dir
-    os.makedirs('plots', exist_ok=True)
+    os.makedirs("plots", exist_ok=True)
 
     # process data and generate the plot
     plot = plt.imshow(wavefield, cmap=cmap)
@@ -18,8 +26,9 @@ def plot_wavefield(wavefield, file_name='wavefield', colorbar=True, cmap="gray",
         divider = make_axes_locatable(ax)
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(plot, cax=cax)
+        plt.clim(clim)
 
-    plt.savefig('plots/{}.png'.format(file_name), format='png')
+    plt.savefig("plots/{}.png".format(file_name), format="png")
 
     if show:
         plt.show()
@@ -28,7 +37,8 @@ def plot_wavefield(wavefield, file_name='wavefield', colorbar=True, cmap="gray",
 
     print("Wavefield saved in plots/{}.png".format(file_name))
 
-def plot_shotrecord(rec, file_name='shotrecord', colorbar=True, show=False):
+
+def plot_shotrecord(rec, file_name="shotrecord", colorbar=True, show=False):
     """
     Plot a shot record (receiver values over time).
 
@@ -37,10 +47,10 @@ def plot_shotrecord(rec, file_name='shotrecord', colorbar=True, show=False):
     rec :
         Receiver data with shape (time, points).
     """
-    scale = np.max(rec) / 10.
+    scale = np.max(rec) / 10.0
     plot = plt.imshow(rec, vmin=-scale, vmax=scale, cmap=cm.gray)
-    plt.xlabel('X position')
-    plt.ylabel('Time')
+    plt.xlabel("X position")
+    plt.ylabel("Time")
 
     # Create colorbar on the right
     if colorbar:
@@ -50,8 +60,8 @@ def plot_shotrecord(rec, file_name='shotrecord', colorbar=True, show=False):
         plt.colorbar(plot, cax=cax)
 
     # create the destination dir
-    os.makedirs('plots', exist_ok=True)
-    plt.savefig('plots/{}.png'.format(file_name), format='png')
+    os.makedirs("plots", exist_ok=True)
+    plt.savefig("plots/{}.png".format(file_name), format="png")
 
     if show:
         plt.show()
@@ -60,10 +70,13 @@ def plot_shotrecord(rec, file_name='shotrecord', colorbar=True, show=False):
 
     print("Shot record saved in plots/{}.png".format(file_name))
 
-def plot_velocity_model(model, file_name='velocity_model', colorbar=True, cmap="jet", show=False):
+
+def plot_velocity_model(
+    model, file_name="velocity_model", colorbar=True, cmap="jet", show=False
+):
 
     # create the destination dir
-    os.makedirs('plots', exist_ok=True)
+    os.makedirs("plots", exist_ok=True)
 
     # process data and generate the plot
     plot = plt.imshow(model.data, cmap=cmap)
@@ -75,7 +88,7 @@ def plot_velocity_model(model, file_name='velocity_model', colorbar=True, cmap="
         cax = divider.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(plot, cax=cax)
 
-    plt.savefig('plots/{}.png'.format(file_name), format='png')
+    plt.savefig("plots/{}.png".format(file_name), format="png")
 
     if show:
         plt.show()
