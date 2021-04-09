8b6b29ec39de0b15b86d9704009ec1b4e653b879
Jaime Freire de Souza <jaimefreire.souza@gmail.com>
2021-04-09 17:38:14 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,26 @@ setup(
     long_description="""Simwave is an hybrid Python/C tool that
     simulates the propagation of the acoustic wave using the
     finite difference method in 2D and 3D domains.""",
+    url='https://github.com/HPCSys-Lab/simwave',
+    download_url=r'''
+    https://github.com/HPCSys-Lab/simwave/archive/refs/tags/v0.5.tar.gz
+    ''',
     author="HPCSys-Lab",
     author_email="senger.hermes@gmail.com",
     license="GPL-3.0 License",
     packages=find_packages(),
     install_requires=required,
+    classifiers=[
+        'Development Status :: 4 - Beta'
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Simulation',
+        'License :: OSI Approved :: GPL-3.0 License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
 )
