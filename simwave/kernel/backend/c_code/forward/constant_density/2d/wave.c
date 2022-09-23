bd36958ad11e1f62cee0241c9250d38fe37b9b54
jaimesouza <jaimefreire.souza@gmail.com>
2022-09-23 10:07:26 -0300

    Update backend
    Update equation formulation in variable density backend code.

--- simwave/kernel/backend/c_code/forward/constant_density/2d/wave.c
+++ simwave/kernel/backend/c_code/forward/constant_density/2d/wave.c
@@ -169,7 +169,7 @@ double forward(f_type *u, f_type *velocity, f_type *damp,
                 // parameter to be used
                 f_type slowness = 1.0 / (velocity[domain_offset] * velocity[domain_offset]);
 
-                //denominator with damp coefficient
+                // denominator with damp coefficient
                 f_type denominator = (1.0 + damp[domain_offset] * dt / (2 * slowness));
                 f_type numerator = (1.0 - damp[domain_offset] * dt / (2 * slowness));
 
@@ -249,7 +249,7 @@ double forward(f_type *u, f_type *velocity, f_type *damp,
                         // parameter to be used
                         f_type slowness = 1.0 / (velocity[domain_offset] * velocity[domain_offset]);
 
-                        //denominator with damp coefficient
+                        // denominator with damp coefficient
                         f_type denominator = (1.0 + damp[domain_offset] * dt / (2 * slowness));
 
                         f_type value = dtSquared / slowness * kws * wavelet[wavelet_offset] / denominator;
